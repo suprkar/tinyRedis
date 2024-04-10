1ee739ddd84ad4004fdc750a831edfc74c25d02e
suprkar <115251486+suprkar@users.noreply.github.com>
2024-04-11 00:39:42 +0530

    Added support for reading and marshalling arrays and strings

--- resp.go
+++ resp.go
@@ -1,69 +1,147 @@
 package main
 
 import(
-    "bufio"
-    "fmt"
-    "strings"
-    "os"
-    "strconv"
+	"bufio"
+	"fmt"
+	"io"
+	"strconv"
 )
 const(
-    STRING='+'
-    ERROR='-'
-    INTEGER=':'
-    BULK='$'
-    ARRAY='*'
+	STRING='+'
+	ERROR='-'
+	INTEGER=':'
+	BULK='$'
+	ARRAY='*'
 
 )
-type value struct{
-    typ string
-    str string
-    num int
-    bulk string
-    array []value
+type Value struct{
+	typ string
+	str string
+	num int
+	bulk string
+	array []Value
 }
 type Resp struct{
-    reader* bufio.Reader
+	reader* bufio.Reader
 }
-func NewResp(rd io.reader) * Resp{
-    return & Resp{reader: bufio.NewReader(rd)
+func NewResp(rd io.Reader) * Resp{
+	return & Resp{reader: bufio.NewReader(rd)}
 }
-func(r* Resp) readLine() (line[] byte,n int,err error){
-    for{
-        b,err:=r.reader.readByte()
-        if err!=nil{
-            return nil,0,err
-        }
-        n+=1
-        line=append(line,b)
-        if len(line)>=2 && line[len(line)-2]=='\r'{
-            break
-        }
-    }
-    return line[:len(line)-2],n,nil
+func(r* Resp) readLine() (line []byte,n int,err error){
+	for{
+		b,err:=r.reader.ReadByte()
+		if err!=nil{
+			return nil,0,err
+		}
+		n+=1
+		line=append(line,b)
+		if len(line)>=2 && line[len(line)-2]=='\r'{
+			break
+		}
+	}
+	return line[:len(line)-2],n,nil
 
 }
-
-func main(){
-    input:="$8\r\nSupratik\r\n"
-    reader:=bufio.NewReader(strings.NewReader(input))
-
-
-    b,_:=reader.ReadByte()
-
-    if b!='$'{
-        fmt.Println("Invalid type,expecting strings only")
-        os.exit(1)
-    }
-    size,_:=reader.ReadByte()
-    strSize,_:=strconv.ParseInt(string(size),10,64)
-
-    //consume \r \n
-    reader.readByte()
-    reader.readByte()
-
-    name:=make([]Byte,strSize)
-    reader.Read(name)
-
-    fmt.Println(string(name))
+func(r *Resp) readInteger() (x int,n int,err error){
+	line,n,err:=r.readLine()
+	if err!=nil{
+		return 0,0,err
+	}
+	i64,err:=strconv.ParseInt(string(line),10,64)
+	if err!=nil{
+		return 0,n,err
+	}
+	return int(i64),n,nil
 }
+func(r *Resp) Read() (Value,error){
+	_type,err:=r.reader.ReadByte()
+	if err!=nil{
+		return Value{},err
+	}
+	switch _type{
+	case ARRAY:
+			return r.readArray()
+	case BULK:
+			return r.readBulk()
+	default:
+			fmt.Printf("Unknown type: %v",string(_type))
+			return Value{},nil
+
+	}
+}
+func(r *Resp) readArray()(Value,error){
+	v:=Value{}
+	v.typ="array"
+	//read length of the array
+
+	len,_,err:=r.readInteger()
+	if err!=nil{
+		return v,err
+	}
+
+	//now for each line we will parse and read the value
+	v.array=make([]Value,0)
+	for i:=0;i<len;i++{
+		val,err:=r.Read()
+		if err!=nil{
+			return v,err
+		}
+		v.array=append(v.array,val)
+
+	}
+	return v,nil
+}
+
+func(r *Resp) readBulk() (Value,error){
+	v:=Value{}
+	v.typ="bulk"
+
+	len,_,err:=r.readInteger()
+	if err!=nil{
+		return v,err
+	}
+	bulk:=make([]byte,len)
+
+	r.reader.Read(bulk)
+	v.bulk=string(bulk)
+	//Now we will read the trailing crlf
+	r.readLine()
+	return v,nil
+}
+
+func(v Value) Marshal() []byte{
+	switch v.typ {
+	case "array":
+			return v.marshalArray()
+	case"bulk":
+			return v.marshalBulk()
+	case "string":
+		return v.marshalString()
+	case "null":
+		return v.marshalNull()
+	case "error":
+		return v.marshalError()
+	default:
+		return []byte{}
+		
+	}
+}
+func(v Value) marshalString() []byte{
+	var bytes []byte
+	bytes=append(bytes,STRING)
+	bytes=append(bytes, v.str...)
+	bytes=append(bytes,'\r','\n')
+
+	return bytes
+}
+func(v Value) marshalBulk() []byte{
+	var bytes []byte
+	bytes=append(bytes,BULK)
+	bytes=append(bytes, strconv.Itoa(len(v.bulk))...)
+	bytes=append(bytes,'\r','\n')
+	bytes=append(bytes,v.bulk...)
+	bytes=append(bytes,'\r','\n')
+
+	return bytes
+}
+
