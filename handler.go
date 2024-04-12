791740d4255875bdef0d79142a22c0110af5d44a
suprkar <115251486+suprkar@users.noreply.github.com>
2024-04-13 01:21:44 +0530

    Added custom functionality for set function

--- handler.go
+++ handler.go
@@ -20,3 +20,22 @@ func ping(args []Value) Value{
 	}
 	return Value{typ:"string",str:args[0].bulk}
 }
+var SETs=map[string]string{}
+var SETsMu=sync.RWMutex{}
+
+func set(args []Value)Value{
+	if len(args)!=2{
+		return Value{typ:"error",str:"ERR wrong number of commands for the set command"}
+	}
+	key:= args[0].bulk
+
+	SETsMu.RLock()
+	value,ok:=SETs[key]
+	SETsMu.RUnlock()
+
+	if !ok{
+		return Value{typ:"null"}
+	}
+
+	return Value{typ:"bulk",bulk:value}
+}
