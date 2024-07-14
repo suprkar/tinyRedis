0adf5b37a8ee48cad491feaf4d5748cbae63c965
suprkar <115251486+suprkar@users.noreply.github.com>
2024-07-15 01:25:30 +0530

    Updated handler to accomadate various commands

--- handler.go
+++ handler.go
@@ -1,41 +1,88 @@
-package main
+import threading
+import resp
 
-import(
-	"sync"
-)
+SETs = {}
+SETsMu = threading.RLock()
 
-var Handlers=map[string]func([]Value) Value{
-	"PING": ping,
-	"SET": set,
-	"GET": get,
-	"HSET": hset,
-	"HGET": hget,
-	"HGETALL": hgetall,
-}
+HSETs = {}
+HSETsMu = threading.RLock()
 
-func ping(args []Value) Value{
-	if len(args)==0{
-		return Value{typ:"string",str:"PONG"}
+def ping(args):
+    if len(args) == 0:
+        return resp.Value(typ="string", str_value="PONG")
+    return resp.Value(typ="string", str_value=args[0].bulk)
 
-	}
-	return Value{typ:"string",str:args[0].bulk}
-}
-var SETs=map[string]string{}
-var SETsMu=sync.RWMutex{}
+def set(args):
+    if len(args) != 2:
+        return resp.Value(typ="error", str_value="ERR wrong number of arguments for 'set' command")
 
-func set(args []Value)Value{
-	if len(args)!=2{
-		return Value{typ:"error",str:"ERR wrong number of commands for the set command"}
-	}
-	key:= args[0].bulk
+    key, value = args[0].bulk, args[1].bulk
 
-	SETsMu.RLock()
-	value,ok:=SETs[key]
-	SETsMu.RUnlock()
+    with SETsMu:  # Changed from SETs_lock
+        SETs[key] = value
 
-	if !ok{
-		return Value{typ:"null"}
-	}
+    return resp.Value(typ="string", str_value="OK")
 
-	return Value{typ:"bulk",bulk:value}
+def get(args):
+    if len(args) != 1:
+        return resp.Value(typ="error", str_value="ERR wrong number of arguments for 'get' command")
+
+    key = args[0].bulk
+
+    with SETsMu:  # Changed from SETs_lock
+        value = SETs.get(key)
+
+    if value is None:
+        return resp.Value(typ="null")
+    return resp.Value(typ="bulk", bulk=value)
+
+def hset(args):
+    if len(args) != 3:
+        return resp.Value(typ="error", str_value="ERR wrong number of arguments for 'hset' command")
+
+    hash_name, key, value = args[0].bulk, args[1].bulk, args[2].bulk
+
+    with HSETsMu:  # Changed from HSETs_lock
+        if hash_name not in HSETs:
+            HSETs[hash_name] = {}
+        HSETs[hash_name][key] = value
+
+    return resp.Value(typ="string", str_value="OK")
+
+def hget(args):
+    if len(args) != 2:
+        return resp.Value(typ="error", str_value="ERR wrong number of arguments for 'hget' command")
+
+    hash_name, key = args[0].bulk, args[1].bulk
+
+    with HSETsMu:  # Changed from HSETs_lock
+        value = HSETs.get(hash_name, {}).get(key)
+
+    if value is None:
+        return resp.Value(typ="null")
+    return resp.Value(typ="bulk", bulk=value)
+
+def hgetall(args):
+    if len(args) != 1:
+        return resp.Value(typ="error", str_value="ERR wrong number of arguments for 'hgetall' command")
+
+    hash_name = args[0].bulk
+
+    with HSETsMu:  # Changed from HSETs_lock
+        hash_map = HSETs.get(hash_name, {})
+
+    array = []
+    for k, v in hash_map.items():
+        array.append(resp.Value(typ="bulk", bulk=k))
+        array.append(resp.Value(typ="bulk", bulk=v))
+
+    return resp.Value(typ="array", array=array)
+
+Handlers = {
+    "PING": ping,
+    "SET": set,
+    "GET": get,
+    "HSET": hset,
+    "HGET": hget,
+    "HGETALL": hgetall
 }
